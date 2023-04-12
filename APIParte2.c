55fe2df6c8f191a7b568f8ba9f734ebf95960215
Milagros Carabelos <mili.carabelos03@gmail.com>
2023-04-11 23:38:56 -0300

    primera idea del orden imparpar a chequear porque los indices son medio confusosss

--- APIParte2.c
+++ APIParte2.c
@@ -4,8 +4,14 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int cmpfunc(const void *a, const void *b) {
+int cmp_ascendente(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
+}
+
+int cmp_descendente(const void *a, const void *b) {
+    int num1 = *((int *)a);
+    int num2 = *((int *)b);
+    return num2 - num1;
 }
 
 u32 Greedy(Grafo G, u32 *Orden, u32 *Color) {
@@ -21,7 +27,7 @@ u32 Greedy(Grafo G, u32 *Orden, u32 *Color) {
     Color[Orden[0]] = 0;
 
     for (u32 i = 1; i < n; i++) {
-        
+
         u32 min_col = 0;
         u32 vertice = Orden[i];
         u32 grado_vertice = Grado(vertice, G);
@@ -32,19 +38,18 @@ u32 Greedy(Grafo G, u32 *Orden, u32 *Color) {
         }
 
         // Ordeno los colores de los vecinos
-        qsort(vecinos_colores, grado_vertice, sizeof(u32), cmpfunc);
+        qsort(vecinos_colores, grado_vertice, sizeof(u32), cmp_ascendente);
 
         if (vecinos_colores[0] == n + 2) {
             Color[vertice] = min_col;
         } else {
 
-            for (j = 0; j < grado_vertice; j++) {
+            for (u32 j = 0; j < grado_vertice; j++) {
                 // empiezo a recorrer desde el color minimo de los vecinos si no es el min_col
                 // resulta que el color es mayor que el min_col, entonces puedo usarlo y hago break
                 if (min_col = vecinos_colores[i]) {
                     min_col++;
-                }
-                else{
+                } else {
                     break;
                 }
             }
@@ -53,10 +58,64 @@ u32 Greedy(Grafo G, u32 *Orden, u32 *Color) {
         }
 
         free(vecinos_colores);
+        vecinos_colores = NULL;
     }
 }
 
 char OrdenImparPar(u32 n, u32 *Orden, u32 *Color) {
+
+    u32 *pares = calloc(n, sizeof(u32));
+    if (pares == NULL) {
+        return (char)1;
+    }
+
+    u32 *impares = calloc(n, sizeof(u32));
+    if (impares == NULL) {
+        free(pares);
+        return (char)1;
+    }
+
+    qsort(Color, n, sizeof(u32), cmp_ascendente);
+
+    u32 last_charged = Color[0];
+    u32 ind_impar = 0;
+
+    for (u32 ind_col = 0; ind_col < n; ind_col++) {
+        u32 ind_par = 0;
+
+        if (Color[ind_col] % 2 == 0 && (Color[ind_col] != last_charged || ind_col == 0)) {
+            pares[ind_par] = Color[ind_col];
+            last_charged = Color[ind_col];
+            ind_par++;
+        }
+        // siempre mi color empieza en 0 asi que no necesito chequear i!=0
+        else if (Color[ind_col] % 2 != 0 && Color[ind_col] != last_charged){
+            impares[ind_impar] = Color[ind_col];
+            last_charged = Color[ind_col];
+            ind_impar++;
+        }
+    }
+
+    u32 i = 0;
+    for (u32 ind = ind_impar - 1; ind >= 0; ind--) {
+
+            Orden[i] = impares[ind];
+            i++;
+    }
+
+    i=0;
+    for (u32 ind = i + 1; ind < n; ind++) {
+
+        Orden[i] = pares[ind];
+        i++;
+    }
+
+    free(pares);
+    pares = NULL;
+    free(impares);
+    impares = NULL;
+
+    return (char)0;
 }
 
 char OrdenJedi(Grafo G, u32 *Orden, u32 *Color) {
